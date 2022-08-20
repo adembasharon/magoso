7fe97717dc9fece30a67288b368b1a9915bf9f49
adembasharon <adembasharon816@gmail.com>
2022-08-20 14:38:14 +0300

    created contactpage and and image folder

---
+++ frontend/pages/contact/contact.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import schoolView from "../../public/images/navBackground.jpg";
+
+const contact=()=>{
+    return(
+        <div>
+
+<div>
+    <Image src={schoolView} alt="" />
+</div>
+
+
+        </div>
+    )
+}
+export default contact
