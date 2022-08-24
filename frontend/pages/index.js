bd75b6319cddb90db450f39170cba8b0a30efb8c
adembasharon <adembasharon816@gmail.com>
2022-08-24 13:00:15 +0300

    added nodemailer

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,9 @@
-import ContactInfo from "./contact/contactInfo";
 import Contact from "./contact/contact"
 export default function Home() {
   return (
     <div>
 <Contact/>
-<ContactInfo/>
+
     </div>
     
   )
